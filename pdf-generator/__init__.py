f2cdc8318650b14ca8e5d57a3096ebd7cdd470c3
Arne Huisman-Aase <arne.huisman.aase@proplan.no>
2022-12-01 08:52:10 +0100

    Changed function name

--- pdf-generator/__init__.py
+++ pdf-generator/__init__.py
@@ -4,9 +4,5 @@ from weasyprint import HTML
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    # name = req.params.get('name')
     pdf = HTML(string=req.get_body().decode('utf-8')).write_pdf()
-
     return func.HttpResponse(pdf)
