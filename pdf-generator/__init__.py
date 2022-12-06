8324d9384ddff81b5a5e9f08edd804047a9f282a
Arne Huisman-Aase <arne.huisman.aase@proplan.no>
2022-12-06 21:25:41 +0100

    Added presentational_hints to pdf generator

--- pdf-generator/__init__.py
+++ pdf-generator/__init__.py
@@ -4,5 +4,5 @@ from weasyprint import HTML
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    pdf = HTML(string=req.get_body().decode('utf-8')).write_pdf()
+    pdf = HTML(string=req.get_body().decode('utf-8')).write_pdf(presentational_hints=true)
     return func.HttpResponse(pdf)
